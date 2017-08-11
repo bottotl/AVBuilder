9f6824f2c3d8948b2db622c452dd91910bd58f51
lintao.yu <lintao.yu@dianping.com>
2017-08-11 19:32:35 +0800

    project init

---
+++ JFTAVBuilder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFTAVBuilder
+//
+//  Created by jft0m on 2017/8/11.
+//  Copyright © 2017年 jft0m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFTAVBuilder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFTAVBuilder
+//
+//  Created by jft0m on 2017/8/11.
+//  Copyright © 2017年 jft0m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
